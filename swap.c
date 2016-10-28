f9e189b45927f427dc0f063542ee749b63553b06
eminecalis <eminecalis@hotmail.de>
2016-10-28 15:42:48 +0200

    new corrected version

--- swap.c
+++ swap.c
@@ -27,19 +27,26 @@ swap_char(char *first_char, char *second_char)
 	*second_char = temp;	
 }
 
-swap_string (char *str1, char *str2)
+swap_string (char **str1, char **str2)
 {
 	char *str_temp = malloc(MAX);
-	strcpy(str_temp,str1);
+/*	strcpy(str_temp,str1);
 	strcpy(str1,str2);
-	strcpy(str2,str_temp);
+	strcpy(str2,str_temp);*/
+    *str_temp = *str1;
+    *str1 = *str2;
+    *str2 = *str1;	
 	
 }
 
-swap_listItem(List *item1, List *item2) 
+swap_listItem(List **item1, List **item2) 
 {
-	List temp;
-	temp.name = item1->name;
+	List *temp;
+	temp = malloc(sizeof(List));
+	*temp = **item1;
+	**item1 = **item2;
+	**item2 = *temp;
+/*	temp.name = item1->name;
 	temp.next = item1->next;
 	temp.prev = item1->prev;
 	item1->name = item2->name;
@@ -47,7 +54,8 @@ swap_listItem(List *item1, List *item2)
 	item1->next = item2->next;
 	item2->name = temp.name;
 	item2->prev = temp.prev;
-	item2->next = temp.next;
+	item2->next = temp.next;*/
+	
 }
 
 int main (int argc, char*argv[])
@@ -67,7 +75,9 @@ int main (int argc, char*argv[])
 	item2->next = "NULL";
 	item1->prev = item1;
 	char str1 [MAX] = "Hello";
-	char str2 [MAX]= "World";	
+	char str2 [MAX]= "World";
+	char *string1 = str1;
+	char *string2 = str2;	
 	printf("Werte vor  dem swapen x: %d und y: %d\n", x, y);
 	swap_int(&x,&y);
 	printf("Werte nach dem swapen x: %d und y: %d\n", x, y);
@@ -75,11 +85,12 @@ int main (int argc, char*argv[])
 	swap_char(&first, &second);
 	printf("Buchstaben nach dem swapen first: %c und second: %c \n", first, second);
 	printf("Listenelemente vor  dem swappen name item1: %s \n                                name item2: %s, \n", item1->name, item2->name);
-	swap_listItem(item1, item2);
+	swap_listItem(&item1, &item2);
 	printf("Listenelemente nach dem swappen name item1: %s \n                                name item2: %s, \n", item1->name, item2->name);
 	printf("Swap von Strings!\n");
-	swap_string(str1, str2);
-	if (strcmp(str1, "World") == 0)
+    swap_string(&string1,&string2);				//wie so funktioniert das nicht mit str1 und str2 ???????
+    
+	if (strcmp(string1, str2) == 0)
 	{
 		printf("Strings erfolgreich getauscht\n");
 		
